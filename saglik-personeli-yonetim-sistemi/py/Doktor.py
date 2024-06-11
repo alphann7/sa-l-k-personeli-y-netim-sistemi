c235785edc5cd2edfb9bf8254ef6e7163636ea74
alphann7 <alphanresmi7@gmail.com>
2024-06-12 00:13:17 +0300

    Create Doktor.py
    doktor sinifi olusturma

---
+++ saglik-personeli-yonetim-sistemi/py/Doktor.py
@@ -0,0 +1,31 @@
+from Personel import Personel
+class Doktor(Personel):
+    def __init__(self, personel_no, ad, soyad, departman, maas, uzmanlik, deneyim_yili, hastane):
+        super().__init__(personel_no, ad, soyad, departman, maas)
+        self.__uzmanlik = uzmanlik
+        self.__deneyim_yili = deneyim_yili
+        self.__hastane = hastane
+
+    def get_uzmanlik(self):
+        return self.__uzmanlik
+
+    def set_uzmanlik(self, uzmanlik):
+        self.__uzmanlik = uzmanlik
+
+    def get_deneyim_yili(self):
+        return self.__deneyim_yili
+
+    def set_deneyim_yili(self, deneyim_yili):
+        self.__deneyim_yili = deneyim_yili
+
+    def get_hastane(self):
+        return self.__hastane
+
+    def set_hastane(self, hastane):
+        self.__hastane = hastane
+
+    def maas_arttir(self, oran):
+        self.__maas *= (1 + oran)
+
+    def __str__(self):
+        return super().__str__() +  f", Uzmanlik: {self.__uzmanlik}, Deneyim Yili: {self.__deneyim_yili}, Hastane: {self.__hastane}"
